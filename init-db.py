b0bb757d0c507beb2cb94606e97c50570e5c749c
carewolf <carewolf@163.com>
2026-07-21 21:13:42 +0800

    更新init-db.py，导入浏览器收藏夹数据
    新增分类：生活、工作、学习
    新增网站链接：23个（来自浏览器收藏夹）
    名称缩短：
    - 中核集团电子采购统一登录 → 中核电子采购
    - 生态环境部核安全执业资格注册办公室 → 核安全注册
    - 中国核能行业协会团体标准信息管理系统 → 核协标准管理
    - 北京市义务教育初中入学服务平台 → 北京义务教育入学
    - 统一认证管理系统 → 统一认证

--- init-db.py
+++ init-db.py
@@ -22,6 +22,7 @@ CREATE TABLE IF NOT EXISTS nav_link (
   title TEXT NOT NULL,
   url TEXT NOT NULL,
   icon TEXT,
+  icon_color TEXT,
   desc TEXT,
   sort INTEGER DEFAULT 0
 )
@@ -47,7 +48,10 @@ db.execute("INSERT OR REPLACE INTO nav_config(key,value) VALUES('admin_pwd','123
 
 db.execute('INSERT INTO nav_category(name,sort) VALUES(?,?)', ('常用工具', 1))
 db.execute('INSERT INTO nav_category(name,sort) VALUES(?,?)', ('技术站点', 2))
-db.execute('INSERT INTO nav_category(name,sort) VALUES(?,?)', ('下载相关', 3))
+db.execute('INSERT INTO nav_category(name,sort) VALUES(?,?)', ('生活', 3))
+db.execute('INSERT INTO nav_category(name,sort) VALUES(?,?)', ('工作', 4))
+db.execute('INSERT INTO nav_category(name,sort) VALUES(?,?)', ('学习', 5))
+db.execute('INSERT INTO nav_category(name,sort) VALUES(?,?)', ('下载相关', 6))
 
 db.execute('INSERT INTO nav_stock(code,name,sort) VALUES(?,?,?)', ('sh000001', '上证指数', 1))
 db.execute('INSERT INTO nav_stock(code,name,sort) VALUES(?,?,?)', ('sz399001', '深证成指', 2))
@@ -56,22 +60,55 @@ db.execute('INSERT INTO nav_stock(code,name,sort) VALUES(?,?,?)', ('sh000300', '
 db.execute('INSERT INTO nav_stock(code,name,sort) VALUES(?,?,?)', ('sh000016', '上证50', 5))
 db.execute('INSERT INTO nav_stock(code,name,sort) VALUES(?,?,?)', ('sh000688', '科创50', 6))
 
-baiduIcon = '<svg viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>'
-googleIcon = '<svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93z"/><path d="M12 4.03c1.1 0 2 .9 2 2v1.16l5.51 2.75.56-1.23C19.45 6.34 16.08 4.03 12 4.03z"/></svg>'
-githubIcon = '<svg viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>'
-defaultIcon = '<svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93z"/></svg>'
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, '百度', 'https://www.baidu.com', 'fa-globe', '#2932E1', '百度搜索', 1))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, 'Google', 'https://www.google.com', 'fa-globe', '#4285F4', 'Google搜索', 2))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, 'GitHub', 'https://github.com', 'fa-github', '#171515', '代码托管平台', 3))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, '微博', 'https://weibo.com', 'fa-globe', '#E6162D', '社交媒体', 4))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, 'B站', 'https://www.bilibili.com', 'fa-globe', '#FB7299', '视频弹幕网站', 5))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, '豆瓣', 'https://www.douban.com', 'fa-book', '#07294D', '书影音社区', 11))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, 'QQ邮箱', 'https://mail.qq.com', 'fa-envelope', '#0099FF', '邮箱服务', 12))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, '阿里云', 'https://www.aliyun.com', 'fa-cloud', '#FF6A00', '云计算平台', 13))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, 'M-Team', 'https://kp.m-team.cc/', 'fa-download', '#c6e985', 'PT站点', 14))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, 'LemonHD', 'https://lemonhd.net/index.php', 'fa-download', '#FFB800', 'PT站点', 15))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (1, 'Transmission', 'http://192.168.31.100:9091/transmission/web/', 'fa-download', '#c563c5', 'BT下载管理', 16))
 
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (1, '百度', 'https://www.baidu.com', baiduIcon, '百度搜索', 1))
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (1, 'Google', 'https://www.google.com', googleIcon, 'Google搜索', 2))
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (1, 'GitHub', 'https://github.com', githubIcon, '代码托管平台', 3))
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (1, '微博', 'https://weibo.com', defaultIcon, '社交媒体', 4))
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (1, 'B站', 'https://www.bilibili.com', defaultIcon, '视频弹幕网站', 5))
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (2, 'MDN', 'https://developer.mozilla.org', defaultIcon, 'Web技术文档', 1))
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (2, 'Stack Overflow', 'https://stackoverflow.com', defaultIcon, '程序员问答社区', 2))
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (2, '掘金', 'https://juejin.cn', defaultIcon, '技术社区', 3))
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (2, 'V2EX', 'https://www.v2ex.com', defaultIcon, '创意工作者社区', 4))
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (2, 'CSDN', 'https://www.csdn.net', defaultIcon, '技术博客平台', 5))
-db.execute('INSERT INTO nav_link(category_id,title,url,icon,desc,sort) VALUES(?,?,?,?,?,?)', (3, 'NovaHD', 'https://pt.novahd.top/index.php', defaultIcon, 'PT下载站点', 1))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (2, 'MDN', 'https://developer.mozilla.org', 'fa-file-code', '#8D67E8', 'Web技术文档', 1))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (2, 'Stack Overflow', 'https://stackoverflow.com', 'fa-code', '#F48024', '程序员问答社区', 2))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (2, '掘金', 'https://juejin.cn', 'fa-gem', '#FF6E40', '技术社区', 3))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (2, 'V2EX', 'https://www.v2ex.com', 'fa-globe', '#666', '创意工作者社区', 4))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (2, 'CSDN', 'https://www.csdn.net', 'fa-globe', '#EB1923', '技术博客平台', 5))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (2, 'React', 'https://react.dev', 'fa-atom', '#61DAFB', 'React框架', 10))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (2, 'Vue', 'https://vuejs.org', 'fa-leaf', '#42B883', 'Vue框架', 11))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (2, 'Webpack', 'https://webpack.js.org', 'fa-gear', '#8DD6F9', '模块打包工具', 12))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (2, 'Vite', 'https://vitejs.dev', 'fa-bolt', '#646CFF', '构建工具', 13))
+
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (3, 'PT邀请码网', 'https://www.ptyqm.com/', 'fa-globe', '#666', 'PT站点邀请码', 1))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (3, 'HDSky', 'https://hdsky.me/login.php', 'fa-globe', '#666', '高清PT站点', 2))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (3, '我的私人导航', 'https://mynav-68k.pages.dev/', 'fa-globe', '#666', '私人导航', 3))
+
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (4, '中核电子采购', 'https://sso.cnncecp.com/login/', 'fa-globe', '#666', '采购系统', 1))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (4, '核安全注册', 'https://www.chinansc.cn/haqgcs/', 'fa-globe', '#666', '核安全注册', 2))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (4, 'EB技术支持', 'https://support.aucotec.cn/web/home/index', 'fa-globe', '#666', '技术支持', 3))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (4, '核协团标公开', 'http://39.104.123.74:8088/standardview/public/index.php/admin/index/index', 'fa-globe', '#666', '标准系统', 4))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (4, '核协标准管理', 'http://39.104.123.74:8088/standard/public/index.php/admin/login.html', 'fa-globe', '#666', '标准管理', 5))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (4, '核协E+', 'https://www.caini.cn/zhhx/1223999/szhzx/1431503/index.html', 'fa-globe', '#666', '移动办公', 6))
+
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, 'MockingBird', 'https://gitee.com/babysor/MockingBird', 'fa-globe', '#666', 'AI拟声', 1))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, '顶牛闯关', 'https://dingniu.suyangx.com/learnspace/learn/learn/xsc/au_steamList.action?params.projectId=402882fd946eb3a601952289139301fd', 'fa-globe', '#666', '学习平台', 2))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, '句乐部', 'https://julebu.co/', 'fa-globe', '#666', '学习社区', 3))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, '玻尔', 'https://www.bohrium.com/', 'fa-globe', '#666', 'AI科学', 4))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, 'Datawhale', 'https://www.datawhale.cn/home', 'fa-globe', '#666', 'AI学习', 5))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, 'uinodes', 'https://uinodes.com/', 'fa-globe', '#666', 'ComfyUI教程', 6))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, 'IAEA CSS', 'https://nucleus.iaea.org/sites/committees', 'fa-globe', '#666', '核安全标准', 7))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, 'SoybeanAdmin', 'https://docs.soybeanjs.cn/zh/', 'fa-globe', '#666', '中后台模板', 8))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, 'Pure Admin', 'https://pure-admin.cn/pages/start/', 'fa-globe', '#666', '后台框架', 9))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, 'RD Web', 'https://cloud.aucotec.cn:888/RDWeb/Pages/zh-CN/login.aspx?ReturnUrl=/RDWeb/Pages/zh-CN/Default.aspx', 'fa-globe', '#666', '远程桌面', 10))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, 'PMP考试', 'https://yun.aura.cn/Packet/detail/lid/764.html', 'fa-globe', '#666', 'PMP学习', 11))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, '百度网盘', 'https://pan.baidu.com/s/1I7wJEasY79IekNO7UMlb-A', 'fa-globe', '#666', '资料下载', 12))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, '北京义务教育入学', 'https://qryjrx.bjedu.cn/hdxsc', 'fa-globe', '#666', '教育平台', 13))
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (5, '统一认证', 'https://am.bjedu.cn/am/UI/Login', 'fa-globe', '#666', '统一认证', 14))
+
+db.execute('INSERT INTO nav_link(category_id,title,url,icon,icon_color,desc,sort) VALUES(?,?,?,?,?,?,?)', (6, 'NovaHD', 'https://pt.novahd.top/index.php', 'fa-download', '#666', 'PT下载站点', 1))
 
 db.commit()
 print('Database initialized successfully')
